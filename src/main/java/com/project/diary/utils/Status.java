0f6d11548589492e36ee766082a989b877a8ed31
sohee <sohee960328@gmail.com>
2020-07-19 18:17:47 +0900

    ADD Status
    status 추가

---
+++ src/main/java/com/project/diary/utils/Status.java
@@ -0,0 +1,14 @@
+package com.project.diary.utils;
+
+public class Status {
+    public static final int OK = 200; //성공
+    public static final int CREATED = 201; //추가 성공
+    public static final int NO_CONTENT = 204; //내용 없음
+    public static final int BAD_REQUEST = 400; //실패
+    public static final int UNAUTHORIZED = 401; //인증 실패
+    public static final int FORBIDDEN = 403; //권한 없음
+    public static final int NOT_FOUND = 404; //찾을 수 없음
+    public static final int INTERNAL_SERVER_ERROR = 500; //서버 내부 에러
+
+    public static final int DB_ERROR = 600; //데이터베이스 에러
+}
