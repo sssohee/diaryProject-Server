6a39bf2420ad14b3563fa4cebb4613505a682ab1
sohee <sohee960328@gmail.com>
2020-07-27 14:34:24 +0900

    MODIFY: user_idx delete

--- src/main/java/com/project/diary/dto/User.java
+++ src/main/java/com/project/diary/dto/User.java
@@ -1,10 +1,9 @@
-﻿package com.project.diary.dto;
+package com.project.diary.dto;
 
 import lombok.Data;
 
 @Data
 public class User {
-    private int user_idx; //회원 고유 번호
     private String user_name; //회원 이름
     private String user_nickname; //회원 닉네임
     private String user_email; //회원 이메일
