b3903d732356f344c157a45f78d6847d4b34c6b7
sohee <sohee960328@gmail.com>
2020-07-30 19:03:05 +0900

    MODIFY: diary_image data type modify, diary_idx add

--- src/main/java/com/project/diary/model/DiaryModifyReq.java
+++ src/main/java/com/project/diary/model/DiaryModifyReq.java
@@ -1,10 +1,12 @@
 package com.project.diary.model;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class DiaryModifyReq {
+    private int diary_idx; //글 고유번호
     private String diary_contents; //글 내용
-    private String diary_image; //이미지
+    private MultipartFile diary_image; //이미지
     private String diary_open; //공개여부
 }
