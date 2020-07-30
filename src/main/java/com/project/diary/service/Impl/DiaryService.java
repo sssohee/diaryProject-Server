1edcaf873624c507b469b11cad766f1f124777a3
sohee <sohee960328@gmail.com>
2020-07-30 19:04:37 +0900

    MODIFY: param add

--- src/main/java/com/project/diary/service/Impl/DiaryService.java
+++ src/main/java/com/project/diary/service/Impl/DiaryService.java
@@ -10,7 +10,13 @@ import com.project.diary.service.DiaryServiceInter;
 import com.project.diary.utils.Message;
 import com.project.diary.utils.Status;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -21,16 +27,46 @@ public class DiaryService implements DiaryServiceInter {
         this.diaryMapper = diaryMapper;
     }
 
+    public class SpringFileWrite {
+        private FileOutputStream fos;
+
+        public void writeFileRename(MultipartFile file, String path, String fileName) {
+            try {
+                byte[] fileData = file.getBytes();
+                fos = new FileOutputStream(path+"\\"+fileName);
+                fos.write(fileData);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     //글 작성
     @Override
-    public DefaultResponse insert(DiaryAddReq diaryAddReq) {
+    public DefaultResponse insert(DiaryAddReq diaryAddReq, String path) {
         try{
             if(diaryAddReq.getUser_idx()<0||diaryAddReq.getDiary_subject().isEmpty()||
                     diaryAddReq.getDiary_contents().isEmpty()||diaryAddReq.getDiary_open().isEmpty()) {
                 return DefaultResponse.res(Status.NO_CONTENT, Message.NO_CONTENT);
             }
 
-            diaryMapper.insert(diaryAddReq);
+            String diary_image = diaryAddReq.getDiary_image().getOriginalFilename();
+
+            if(!diary_image.equals("")){
+                //실제 폴더에 이미지 저장
+                SpringFileWrite sfw = new SpringFileWrite();
+                sfw.writeFileRename(diaryAddReq.getDiary_image(), path, diary_image);
+            }else
+                diary_image = "0";
+
+            diaryMapper.insert(diaryAddReq, diary_image);
+
             if(diaryMapper.userNewDiary(diaryAddReq.getUser_idx()).getDiary_contents().equals(diaryAddReq.getDiary_contents()))
                 return DefaultResponse.res(Status.CREATED, Message.ADD_DIARY_SUCCESS);
             else
@@ -43,16 +79,31 @@ public class DiaryService implements DiaryServiceInter {
 
     //글 수정
     @Override
-    public DefaultResponse update(DiaryModifyReq diaryModifyReq, int diary_idx) {
+    public DefaultResponse update(DiaryModifyReq diaryModifyReq, String path) {
         try{
             if(diaryModifyReq.getDiary_contents().isEmpty()||diaryModifyReq.getDiary_open().isEmpty()) {
                 return DefaultResponse.res(Status.NO_CONTENT, Message.NO_CONTENT);
             }
 
-            diaryMapper.update(diaryModifyReq, diary_idx);
-            final Diary diary = diaryMapper.getDiary(diary_idx);
+            String diary_image = diaryModifyReq.getDiary_image().getOriginalFilename();
 
-            if(diary.getDiary_contents().equals(diaryModifyReq.getDiary_contents()) && diary.getDiary_image().equals(diaryModifyReq.getDiary_image())
+            if(!diary_image.equals("")){
+                //이미지가 존재할경우 삭제
+                File file = new File(path+"\\"+diary_image);
+                if(file.exists())
+                    file.delete();
+
+                //실제 폴더에 이미지 저장
+                SpringFileWrite sfw = new SpringFileWrite();
+                sfw.writeFileRename(diaryModifyReq.getDiary_image(), path, diary_image);
+            }else
+                diary_image = "0";
+
+            diaryMapper.update(diaryModifyReq, diary_image);
+
+            final Diary diary = diaryMapper.getDiary(diaryModifyReq.getDiary_idx());
+
+            if(diary.getDiary_contents().equals(diaryModifyReq.getDiary_contents()) && diary.getDiary_image().equals(diary_image)
                     && diary.getDiary_open().equals(diaryModifyReq.getDiary_open()))
                 return DefaultResponse.res(Status.CREATED, Message.MODIFY_DIARY_SUCCESS);
             else
@@ -65,8 +116,16 @@ public class DiaryService implements DiaryServiceInter {
 
     //글 삭제
     @Override
-    public DefaultResponse delete(int diary_idx, int user_idx) {
+    public DefaultResponse delete(int diary_idx, int user_idx, String path) {
         try{
+            //이미지 파일명
+            String diary_image = diaryMapper.getDiary(diary_idx).getDiary_image();
+
+            //이미지가 존재할경우 삭제
+            File file = new File(path+"\\"+diary_image);
+            if(file.exists())
+                file.delete();
+
             if(user_idx==1){
                 diaryMapper.deleteAdmin(diary_idx);
                 if(diaryMapper.getDiary(diary_idx).getDiary_status()==1)
