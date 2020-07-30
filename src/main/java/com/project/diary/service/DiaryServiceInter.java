c2be478a4a4d1a3ef79d5abf1b4387d0380475d1
sohee <sohee960328@gmail.com>
2020-07-30 19:04:34 +0900

    MODIFY: param add

--- src/main/java/com/project/diary/service/DiaryServiceInter.java
+++ src/main/java/com/project/diary/service/DiaryServiceInter.java
@@ -3,11 +3,12 @@ package com.project.diary.service;
 import com.project.diary.model.DefaultResponse;
 import com.project.diary.model.DiaryAddReq;
 import com.project.diary.model.DiaryModifyReq;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 public interface DiaryServiceInter {
-    DefaultResponse insert(final DiaryAddReq diaryAddReq);
-    DefaultResponse update(final DiaryModifyReq diaryModifyReq, final int diary_idx);
-    DefaultResponse delete(final int diary_idx, final int user_idx);
+    DefaultResponse insert(final DiaryAddReq diaryAddReq, final String path);
+    DefaultResponse update(final DiaryModifyReq diaryModifyReq, final String path);
+    DefaultResponse delete(final int diary_idx, final int user_idx, final String path);
 
     DefaultResponse diaryDateCount(final int user_idx);
 
@@ -15,4 +16,5 @@ public interface DiaryServiceInter {
 
     DefaultResponse userDiaryList(final int user_idx);
     DefaultResponse getDiary(final int diary_idx);
+
 }
